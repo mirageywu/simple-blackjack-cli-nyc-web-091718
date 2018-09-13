1b7412bfe94204f1678590493429129aff9a849f
Yashih Wu <yashih.wu@gmail.com>
2018-09-13 05:11:18 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,8 +55,8 @@ def runner
   card_total = initial_round
   until card_total > 21 do
     hit?(card_total)
-    display_card_total(card_total)
     return(card_total)
+    display_card_total(card_total)
   end
   end_game(card_total)
 end
